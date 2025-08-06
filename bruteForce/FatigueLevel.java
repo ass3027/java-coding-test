eed1fc92a6079d9063fe776812edc82d52c00673
ass3027 <blackfeather227@gmail.com>
2025-08-06 14:28:54 +0900

    refactor(DFS): FatigueLevel, recursive

--- bruteForce/FatigueLevel.java
+++ bruteForce/FatigueLevel.java
@@ -8,19 +8,43 @@ public class FatigueLevel {
     public static void main(String[] args) {
         new Solution().solution(0, new int[][]{});
     }
+
     static class Solution {
+        int[][] dungeons;
+        int IS_VISITED = -1;
+        public int solution(int k, int[][] dungeons) {
+            this.dungeons = dungeons;
+            return dfs(k, 0);
+        }
+
+        int dfs(int health, int current) {
+            int max = current;
+            for (int i = 0; i < dungeons.length; i++) {
+                if (dungeons[i][0] == IS_VISITED || dungeons[i][0] > health)
+                    continue;
+
+                int temp = dungeons[i][0];
+                dungeons[i][0] = IS_VISITED;
+                max = Math.max(max, dfs(health - dungeons[i][1], current + 1));
+                dungeons[i][0] = temp;
+            }
+            return max;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    static class SolutionBefore {
         int max = 0;
         int health = 0;
         int[][] dungeons;
         public int solution(int k, int[][] dungeons) {
             health = k;
-            Stack<Integer> st = new Stack<>();
             this.dungeons = dungeons;
-            bfs(st);
+            dfs(new Stack<>());
             return max;
         }
 
-        void bfs(Stack<Integer> st){
+        void dfs(Stack<Integer> st){
             List<Integer> canGo = getCanGo(st);
             if(canGo.isEmpty()){
                 max = Math.max(max, st.size());
@@ -29,18 +53,19 @@ public class FatigueLevel {
             }
             for(int i : canGo){
                 st.push(i);
-                // health -= dungeons[i][1];
-                bfs(st);
+                health -= dungeons[i][1];
+                dfs(st);
             }
-            health += dungeons[st.pop()][1];
+            if(!st.isEmpty())
+                health += dungeons[st.pop()][1];
         }
 
         List<Integer> getCanGo(Stack<Integer> st){
             List<Integer> result = new ArrayList<>();
-            for(int i=0; i<dungeons.length; i++){
+            for(int i=0; i<dungeons.length; i++)
                 if(!st.contains(i) && dungeons[i][0] <= health)
                     result.add(i);
-            }
+
             return result;
         }
     }
