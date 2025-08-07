0ace39a42cabb301408c2bbb3ae087e3390a13e4
ass3027 <blackfeather227@gmail.com>
2025-08-07 20:12:53 +0900

    fail(OverlapSecMin): solving

--- beforeClassification/OverlapSecMin.java
+++ beforeClassification/OverlapSecMin.java
@@ -6,6 +6,29 @@ public class OverlapSecMin {
         new Solution().solution(0,5,30,0,7,0);
     }
 
+    static class Solution2 {
+
+        public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
+//            int[] start = new int[]{h1, m1, s1};
+//            int[] middleStart =  new int[]{h1, m1 + s1 == 0 ? 0 : 1, s1};
+//            int[] middleEnd = new int[]{h2, m2, s2};
+//            int[] end = new int[]{h2, m2, s2};
+            int m1m = m1 + (s1 == 0 ? 0 :1);
+            int h1m = h1 + (m1m == 60 ? 1 : 0);
+            m1m = m1m % 60;
+
+            int startCount = (s1 <= h1 * 5 ? 1 : 0) + (s1 <= m1 ? 1 : 0);
+            int endCount = (s2 <= h2 * 5 ? 1 : 0) + (s2 <= m2 ? 1 : 0);
+
+            boolean has12 = h1m < 12 && h2 >= 12 || h1m == 12 && m1m == 0 || h2 == 12 && m2 == 0;
+            int countOfMinute = ((h2 * 60 + m2) - (h1 * 60 + m1)) * 2;
+            int middleCount = countOfMinute < 0 ? 0 : countOfMinute
+                    + (h1 == 0 && m1 == 0 ? 1 : 0)
+                    + (has12  ? 1 : 0);
+            return startCount + endCount + middleCount;
+        }
+    }
+
     static class Solution {
 
         int startTime;
